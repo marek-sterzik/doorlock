5325f64253391a7c779cd63d20ccddca2565dc76
Marek Sterzik <marek@milimetr.org>
2021-05-07 09:36:22 +0200

    added fixtures

--- fixtures/data.sql
+++ fixtures/data.sql
@@ -21,6 +21,7 @@
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` (`id`, `login`, `password`) VALUES (1,'franta.vomacka','tajneheslo');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -33,4 +34,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-04-23  8:16:35
+-- Dump completed on 2021-05-07  7:32:36
